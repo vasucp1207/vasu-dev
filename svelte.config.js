6fbc3cb4743f41200cfcaf092788a63c348468f9
vasucp1207 <vasucp1207@gmail.com>
2023-07-28 10:08:37 +0530

    minimal

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-import{ mdsvex }from'mdsvex';
-import mdsvexConfig from'./mdsvex.config.mjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions:['.svelte', '.svx', '.svelte.md'],
-	preprocess:[mdsvex(mdsvexConfig)],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
